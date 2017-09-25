ae3f3f6f628f50ed5a39bb44683928b70a23d3c9
Ryan Prairie <prairie421@gmail.com>
2017-09-25 18:08:31 -0400

    added boundarie support

--- nicolesGame.py
+++ nicolesGame.py
@@ -13,47 +13,55 @@ gameDisplay= pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('N + R = <3')
 clock = pygame.time.Clock()
 
+
 bunnyImg = pygame.image.load('data/images/bunny.png')
 
+pygame.display.set_icon(pygame.image.load('data/images/icon.png'))
 
 def bunny(x, y):
     gameDisplay.blit(bunnyImg, (x, y))
 
-x = (display_width * 0.48)
-y = (display_height * 0.48)
+def game_loop():
+    x = (display_width * 0.48)
+    y = (display_height * 0.48)
+    
+    bunny_width = 65
+    x_change = 0
+    y_change = 0
 
-x_change = 0
-y_change = 0
+    finished = False
 
-finished = False
+    while not finished:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
 
-while not finished:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -4
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 4
+                if event.key == pygame.K_UP:
+                    y_change = -4
+                elif event.key == pygame.K_DOWN:
+                    y_change = 4
+
+            if event.type == pygame.KEYUP:
+                if event.key in (pygame.K_LEFT, pygame.K_RIGHT): 
+                    x_change = 0
+                if event.key in (pygame.K_UP, pygame.K_DOWN):
+                    y_change = 0
+
+        x += x_change
+        y += y_change
+        gameDisplay.fill(white)
+        bunny(x, y + y_change)
+        if x > display_width - bunny_width or x < 0:
             finished = True
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -4
-            elif event.key == pygame.K_RIGHT:
-                x_change = 4
-            if event.key == pygame.K_UP:
-                y_change = -4
-            elif event.key == pygame.K_DOWN:
-                y_change = 4
+        pygame.display.update()
+        clock.tick(60)
 
-        if event.type == pygame.KEYUP:
-            if event.key in (pygame.K_LEFT, pygame.K_RIGHT): 
-                x_change = 0
-            if event.key in (pygame.K_UP, pygame.K_DOWN):
-                y_change = 0
-
-    x += x_change
-    y += y_change
-    gameDisplay.fill(white)
-    bunny(x, y + y_change)
-    pygame.display.update()
-    clock.tick(60)
-
+game_loop()
 pygame.quit()
 quit()

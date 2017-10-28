27a6af83980b72d0d83efba75e928f2daab5ada6
Ryan Prairie <prairie421@gmail.com>
2017-10-28 14:21:28 -0400

    added more of a framework and the box object

--- boxs.py
+++ boxs.py
@@ -1,6 +1,6 @@
-import pygame
-import time
 import sys
+import time
+import pygame
 
 from pygame.locals import *
 
@@ -9,8 +9,10 @@ clock=pygame.time.Clock()
 
 display_width, display_height = 640, 480
 screen = pygame.display.set_mode((display_width, display_height))
-surface = pygame.Surface(screen.get_size()) 
+surface = pygame.Surface(screen.get_size())
+
 pygame.display.set_caption('basic box prototype')
+boxImg = pygame.image.load('data/images/box.png')
 
 pygame.key.set_repeat(1, 40) #checks the keys press (delay, interval)
 
@@ -19,16 +21,39 @@ class Box(object):
     """The Box object for a box moving game"""
 
     def __init__(self):
+        """TODO: 
+            movement with the bunny if they are touching. 
+            check if touchs bunny
+            check if touchs goal
+            """
+        self.posX = 0
+        self.posY = 60
+
+    def draw(self, x, y):
+        screen.blit(boxImg, (self.posX + x, self.posY + y))
+
+class Nicole(object):
+
+    """Docstring for Nicole. """
+
+    def __init__(self):
         """TODO: to be defined1. """
+        
+class Goal(object):
 
+    """Docstring for Goal. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        
 if __name__ == "__main__":
-       box = Box()
+    box = Box()
+    box.draw(0, 0)
+    while True:
 
-       while True:
-
-           for event in pygame.event.get():
-               if event.type == pygame.QUIT:
-                   pygame.quit()
-                   sys.exit()
-
-           clock.tick(120)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+        clock.tick(120)

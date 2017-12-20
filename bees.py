f756bbe5ff3bb64de8be065e55423689448db44f
Ryan Prairie <prairie421@gmail.com>
2017-12-20 15:37:04 -0500

    added goal and bunny object

--- bees.py
+++ bees.py
@@ -46,3 +46,49 @@ class Honey(object):
     def collidewith(self, ob):
         return self.rect.colliderect(ob)
 
+class Bunny(object):
+
+    """Docstring for Bunny. """
+
+    def __init__(self):
+        """TODO: 
+            movement 
+            """
+        self.x = 75
+        self.y = display_height / 2
+        self.image = bunnyImg
+        self.rect = 0
+        self.height = self.image.get_height()
+        self.width = self.images.get_width()
+        self.topx = 0
+        self.topy = 0
+
+    def draw(self, x, y):
+        screen.blit(bunnyImg, (self.x + x, self.y + y))
+        self.topx = ((self.x + x) - (self.width / 2) + 1)
+        self.topy = ((self.y + y) - (self.height / 2) + 1)
+        self.rect = pygame.Rect((self.topx, self.topy), (self.width, self.height))
+
+    def collidewith(self, ob):
+        return self.rect.collidewith(ob)
+
+class Goal(object):
+
+    """Docstring for Goal. """
+
+    def __init__(self):
+        """TODO: check if honey gets to goal. """
+        self.x = 50
+        self.y = display_height / 2
+        self.image = goalImg
+        self.rect = 0
+        self.height = self.image.get_height()
+        self.width = self.images.get_width()
+        self.topx = 0
+        self.topy = 0
+
+    def draw(self):
+        screen.blit(bunnyImg, (self.x, self.y))
+        self.topx = ((self.x) - (self.width / 2) + 1)
+        self.topy = ((self.y) - (self.height / 2) + 1)
+        self.rect = pygame.Rect((self.topx, self.topy), (self.width, self.height))
